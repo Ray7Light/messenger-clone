ad6bd6a8177a8216ee8a870c009aca935ad64758
Marcin <m.ficek@infomex.pl>
2023-05-31 15:00:12 +0200

    + registration functionality

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,115 +1,119 @@
 'use client';
 
-import { useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import axios from 'axios';
+import { useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
 
-import Button from "@/app/components/Button";
-import Input from "@/app/components/inputs/Input";
+import Button from '@/app/components/Button';
+import Input from '@/app/components/inputs/Input';
 
-import AuthSocialButton from "./AuthSocialButton";
+import AuthSocialButton from './AuthSocialButton';
 
 enum VariantEnum {
   LOGIN = 'LOGIN',
-  REGISTER = 'REGISTER'
+  REGISTER = 'REGISTER',
 }
 
 const AuthForm = () => {
   const [variant, setVariant] = useState<VariantEnum>(VariantEnum.LOGIN);
   const [isLoading, setIsLoading] = useState(false);
-  const { register, handleSubmit, formState: {
-    errors
-  } } = useForm<FieldValues>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
     defaultValues: {
       name: '',
       email: '',
-      password: ''
-    }
-  })
+      password: '',
+    },
+  });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
     if (variant === VariantEnum.REGISTER) {
-      // Axios Register
+      axios.post('/api/register', data);
     }
 
     if (variant === VariantEnum.LOGIN) {
       // NextAuth SignIn
     }
-  }
+  };
 
   return (
-    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+    <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+      <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+        <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
           {variant === VariantEnum.REGISTER && (
             <Input
-              id="name"
-              label="Name"
+              id='name'
+              label='Name'
               disabled={isLoading}
               register={register}
               errors={errors}
             />
           )}
           <Input
-            id="email"
-            label="Email address"
-            type="email"
+            id='email'
+            label='Email address'
+            type='email'
             disabled={isLoading}
             register={register}
             errors={errors}
           />
           <Input
-            id="password"
-            label="Password"
-            type="password"
+            id='password'
+            label='Password'
+            type='password'
             disabled={isLoading}
             register={register}
             errors={errors}
           />
           <div>
-            <Button
-              disabled={isLoading}
-              fullWidth
-              type="submit"
-            >
-              {variant === VariantEnum.LOGIN ? "Sign in" : "Register"}
+            <Button disabled={isLoading} fullWidth type='submit'>
+              {variant === VariantEnum.LOGIN ? 'Sign in' : 'Register'}
             </Button>
           </div>
         </form>
 
-        <div className="mt-6">
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-gray-300" />
+        <div className='mt-6'>
+          <div className='relative'>
+            <div className='absolute inset-0 flex items-center'>
+              <div className='w-full border-t border-gray-300' />
             </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="bg-white px-2 text-gray-500">
+            <div className='relative flex justify-center text-sm'>
+              <span className='bg-white px-2 text-gray-500'>
                 Or continue with
               </span>
             </div>
           </div>
-          <div className="mt-6 flex gap-2">
-            <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')} />
-            <AuthSocialButton icon={BsGoogle} onClick={() => socialAction('google')} />
+          <div className='mt-6 flex gap-2'>
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
           </div>
         </div>
 
-        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+        <div className='flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500'>
           <div>
-            {variant === VariantEnum.LOGIN ? 'New to Messenger?' : 'Already have an account?'}
+            {variant === VariantEnum.LOGIN
+              ? 'New to Messenger?'
+              : 'Already have an account?'}
           </div>
-          <div
-            className="underline cursor-pointer"
-            onClick={toggleVariant}
-          >
+          <div className='underline cursor-pointer' onClick={toggleVariant}>
             {variant === VariantEnum.LOGIN ? 'Create an account' : 'Login'}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 
   function socialAction(action: string) {
     setIsLoading(true);
@@ -124,8 +128,8 @@ const AuthForm = () => {
       }
 
       return VariantEnum.LOGIN;
-    })
+    });
   }
-}
+};
 
 export default AuthForm;
